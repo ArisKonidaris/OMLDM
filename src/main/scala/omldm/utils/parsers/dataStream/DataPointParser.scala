6319bb7b45f24f4c1618f6835e796db7c157a214
aris <v.b.konidaris@gmail.com>
2020-07-29 20:23:20 +0300

    Added prediction nodes.

--- src/main/scala/omldm/utils/parsers/dataStream/DataPointParser.scala
+++ src/main/scala/omldm/utils/parsers/dataStream/DataPointParser.scala
@@ -1,7 +1,7 @@
 package omldm.utils.parsers.dataStream
 
 import ControlAPI.DataInstance
-import mlAPI.math.{DenseVector, LabeledPoint, Point, UnlabeledPoint}
+import mlAPI.math.{DenseVector, LabeledPoint, Point, UnlabeledPoint, Vector}
 import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.util.Collector
@@ -15,20 +15,39 @@ class DataPointParser() extends RichFlatMapFunction[DataInstance, Point] {
 
   override def flatMap(input: DataInstance, collector: Collector[Point]): Unit = {
 
-    //TODO: Remove this line after the implementation of ml methods that use Discrete Features.
-    if (input.getNumericFeatures == null || input.getDiscreteFeatures != null) return
+    // TODO: Remove this line after the implementation of ML methods that use Discrete Features.
+    if (input.getNumericFeatures == null || input.getDiscreteFeatures != null || input.getCategoricalFeatures != null)
+      return
 
     {
       if (input.getOperation.equals("training")) {
-        val numericData = DenseVector(input.getNumericFeatures.asInstanceOf[java.util.List[Double]].asScala.toArray)
+
+        val features: (Vector, Vector, Array[String]) = {
+          (
+            if (input.getNumericFeatures == null)
+              DenseVector()
+            else
+              DenseVector(input.getNumericFeatures.asInstanceOf[java.util.List[Double]].asScala.toArray),
+            if (input.getDiscreteFeatures == null)
+              DenseVector()
+            else
+              DenseVector(input.getDiscreteFeatures.asInstanceOf[java.util.List[Int]].asScala.toArray.map(x => x.toDouble)),
+            if (input.getCategoricalFeatures == null)
+              Array[String]()
+            else
+              input.getCategoricalFeatures.asScala.toArray
+          )
+        }
+
         if (input.getTarget != null)
-          Some(LabeledPoint(input.getTarget, numericData))
+          Some(LabeledPoint(input.getTarget, features._1, features._2, features._3))
         else
-          Some(UnlabeledPoint(numericData))
+          Some(UnlabeledPoint(features._1, features._2, features._3))
+
       } else None
     } match {
       case Some(point: Point) => collector.collect(point)
-      case _ => println("Unknown DataInstance type")
+      case _ => println("Unknown DataInstance type.")
     }
 
   }
