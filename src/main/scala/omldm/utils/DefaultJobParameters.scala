761f88295eccd3e7313671f907e1ebc0e87e5e87
aris <v.b.konidaris@gmail.com>
2020-09-08 14:38:22 +0300

    Updated DataInstance to support metadata and make the SVM classifier available to the user to train.

--- src/main/scala/omldm/utils/DefaultJobParameters.scala
+++ src/main/scala/omldm/utils/DefaultJobParameters.scala
@@ -5,5 +5,5 @@ object DefaultJobParameters {
   val defaultParallelism: String = "16"
   val defaultInputFile: String = "hdfs://clu01.softnet.tuc.gr:8020/user/vkonidaris/lin_class_mil_e10.txt"
   val defaultOutputFile: String = "hdfs://clu01.softnet.tuc.gr:8020/user/vkonidaris/output"
-  val defaultTestParameter: String = "true"
+  val defaultTestParameter: String = "false"
 }
