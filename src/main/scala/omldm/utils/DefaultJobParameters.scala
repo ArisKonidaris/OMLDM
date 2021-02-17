920b7c9dc9341589b9c6b8eff218c127c90eaec0
ArisKonidaris <KimiR@if2007!>
2021-02-17 03:02:14 +0200

    1) Extended GM and FGM protocols with parallel parameter servers.

--- src/main/scala/omldm/utils/DefaultJobParameters.scala
+++ src/main/scala/omldm/utils/DefaultJobParameters.scala
@@ -3,7 +3,7 @@ package omldm.utils
 object DefaultJobParameters {
   val defaultJobName: String = "OML_job_1"
   val defaultParallelism: String = "16"
-  val defaultMaxMsgParams: String = "10000"
+  val defaultMaxMsgParams: String = "2000"
   val defaultInputFile: String = "hdfs://clu01.softnet.tuc.gr:8020/user/vkonidaris/lin_class_mil_e10.txt"
   val defaultOutputFile: String = "hdfs://clu01.softnet.tuc.gr:8020/user/vkonidaris/output"
   val defaultTestParameter: String = "true"
